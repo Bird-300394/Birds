1c11091ae20d4efd96caa1c1af575ec6c4480692
kirill <kir.prsv@gmail.com>
2022-12-08 19:09:45 +0300

    +Kiwi

--- Birds1/src/Kiwi.java
+++ Birds1/src/Kiwi.java
@@ -1,2 +1,9 @@
-public class Kiwi {
+public class Kiwi extends Bird {
+    String lifestyle;
+    String color;
+
+    public Kiwi(String lifestyle, String color) {
+        this.lifestyle=lifestyle;
+        this.color=color;
+    }
 }
