04fb9794b78fe80cd59de92e24650c374bd1e2c0
Vasya <vasya.454@gmail.com>
2022-12-08 11:47:08 +0300

    Birds и дятел

---
+++ Birds1/src/Birds.java
@@ -0,0 +1,23 @@
+public class Birds {
+    public String species;
+    public String name;
+    public int age;
+    protected double weight;
+    boolean flyable;
+    final boolean FEATHERS = true;
+    final boolean BEAK = true;
+
+    public boolean hasFeathers() {
+        return FEATHERS;
+    }
+
+    public boolean hasBeak() {
+        return BEAK;
+    }
+    public void setFlyable(boolean f) {
+        flyable = f;
+    }
+    public boolean isFlyable() {
+        return flyable;
+    }
+}
---
+++ Birds1/src/Woodpecker.java
@@ -0,0 +1,34 @@
+public class Woodpecker extends Birds {
+    public double lucky = 0.05;
+    public Woodpecker() {
+        this.species = "Дятел";
+        this.flyable = true;
+    }
+    public Woodpecker(String name, int age, double weight) {
+        this.species = "Дятел";
+        this.flyable = true;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public void knocks() {
+        try {
+            while (true) {
+                if (Math.random() < 1 - lucky) {
+                    System.out.println("Дятел продолжает стучать!");
+                } else {
+                    System.out.println("Дятел находит червяка и перелетает на другое дерево");
+                    break;
+                }
+                Thread.sleep(444);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Дятел достучался");
+        }
+    }
+//    Для теста:
+//    Woodpecker woody = new Woodpecker("Woody",32,1.8);
+//    woody.knocks();
+}
