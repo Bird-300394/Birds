1a747b9d7883fd5afe564adbe85c0778f0f2ccea
Bird-300394 <116818136+Bird-300394@users.noreply.github.com>
2022-12-08 16:55:45 +0300

    + добавил умения и голос

--- Birds1/src/Lark.java
+++ Birds1/src/Lark.java
@@ -1,15 +1,25 @@
 public class Lark {
   String Name;
   String Fly;
+  String  Sing;
   int Age;
 
- public Lark (String Name,String Fly,int Age){
+ public Lark (String Name,String Fly,String Sing,int Age){
      this.Name=Name;
      this.Fly=Fly;
+     this.Sing=Sing;
      if (Age<0)
          this.Age=0;
      else
          this.Age=Age;
 
  }
+ public void Fly (){
+     System.out.println("Улетает на юг");
+ }
+ public  void Sing(){
+     System.out.println("Чрик-чрик-чрик");
+
+ }
+
 }
